20c262015d7cd3e570fa3f245f8f1ecfd92ca3f5
tusharr-patil <patiltusharvikas@gmail.com>
2024-01-14 22:43:52 +0530

    soc for graph and value

--- backprop.py
+++ backprop.py
@@ -1,4 +1,4 @@
-from graphviz import Digraph
+import graph
 
 
 # Equivalent to Tensor in pytorch
@@ -90,51 +90,6 @@ class Value:
             node._backward()
 
 
-# Help us to draw graw of tensors
-class Graph:
-    # Draw the Graph
-    @staticmethod
-    def trace(root):
-        # builds a set of all nodes and edges in a graph
-        nodes, edges = set(), set()
-
-        def build(v):
-            if v not in nodes:
-                nodes.add(v)
-                for child in v._parent:
-                    edges.add((child, v))
-                    build(child)
-
-        build(root)
-        return nodes, edges
-
-    @staticmethod
-    def draw_dot(root):
-        dot = Digraph(format="svg", graph_attr={"rankdir": "LR"})  # LR = left to right
-
-        nodes, edges = Graph.trace(root)
-        for n in nodes:
-            uid = str(id(n))
-            # for any value in the graph, create a rectangular ('record') node for it
-            dot.node(
-                name=uid,
-                label="{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad),
-                shape="record",
-            )
-            if n._op:
-                # if this value is a result of some operation, create an op node for it
-                dot.node(name=uid + n._op, label=n._op)
-                # and connect this node to it
-                dot.edge(uid + n._op, uid)
-
-        for n1, n2 in edges:
-            # connect n1 to the op node of n2
-            dot.edge(str(id(n1)), str(id(n2)) + n2._op)
-
-        with open("output_graph.svg", "wb") as f:
-            f.write(dot.pipe(format="svg"))
-
-
 # Neuron
 # wx = w * x
 # wx + b = L
@@ -159,4 +114,4 @@ L.label = "L"
 L.grad = 1
 L.back_propogation()
 
-Graph.draw_dot(L)
+graph.draw_dot(L)
